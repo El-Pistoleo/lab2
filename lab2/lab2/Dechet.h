59e9bfd027d6ec4133afddf8f428aa223adf368e
Léo Le Corre <leo.le-corre1@uqac.ca>
2018-10-04 14:49:03 -0500

    classe dechet

---
+++ lab2/lab2/Dechet.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream> //pour les string
+
+using namespace std;
+
+class Dechet
+{
+public:
+	Dechet();
+	~Dechet();
+	int getPoids();
+	int getId();
+	string getDescription();
+	int getType();
+	string getCouleur();
+	Materiel getMateriel();
+	int getPurete();
+	bool estEnStyromousse();
+	bool estRigide();
+	friend ostream& operator << (ostream& out, const Dechet& dechet);
+
+protected:
+	Dechet(int poids, string description, int type, string couleur, Materiel materiel, int purete, bool estEnStyromousse, bool rigide);
+	Dechet(int poids, string description);
+
+private:
+	int poids;
+	int id;
+	string description;
+	int type;
+	string couleur;
+	Materiel materiel;
+	int purete;
+	bool styromousse;
+	bool rigide;
+	int idCourant;
+
+	void setPoids(int poids);
+	void setDescription(string description);
+	void setType(int type);
+	void setClouleur(string couleur);
+	void setPurete(int purete);
+
+	enum Materiel { CARTON, BOUTEILLE, PLASTIQUE, METAL, BOIS, NOURRITURE, VEGETAL, PAPIER, STYROFOAM, INCONNU };
+};
